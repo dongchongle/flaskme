0a9edeb6f710be1d4d77a6dc8b0ae569ef5c5b00
VimDong <dongchongle@gmail.com>
2016-04-19 18:09:51 +0800

    Flask-Bootstrap

--- hello.py
+++ hello.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Flask
-from flask.ext.script import Manager
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+# from flask.ext.script import Manager
 
 app = Flask(__name__)
-manager = Manager(app)
+bootstrap = Bootstrap(app)
+# manager = Manager(app)
+
+# @app.route('/')
+# def index():
+#     return '<h1>Hello World</h1>'
+
+# @app.route('/user/<name>')
+# def user(name):
+#     return '<h1>Hello %s</h1>' % name
 
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello %s</h1>' % name
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    manager.run()
+    app.run(debug=True)
+    # manager.run()
