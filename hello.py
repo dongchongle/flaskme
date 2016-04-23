cb1d9a7d9d03da01ba3e74f38adffbcce2e646b3
VimDong <dongchongle@gmail.com>
2016-04-23 17:42:32 +0800

    Flash

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from datetime import datetime
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 
@@ -31,12 +31,14 @@ class NameForm(Form):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-        name = None
-        form = NameForm()
-        if form.validate_on_submit():
-            session['name'] = form.name.data
-            return redirect(url_for('index'))
-        return render_template('index.html', form=form, name=session.get('name'))
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 # @app.route('/user/<name>')
 # def user(name):
