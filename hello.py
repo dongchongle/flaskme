82a57e2ad304b13d8bb3ee6614befc850ecade68
VimDong <dongchongle@gmail.com>
2016-04-26 16:19:41 +0800

    deleted code

--- hello.py
+++ hello.py
@@ -19,15 +19,6 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
-# manager = Manager(app)
-
-# @app.route('/')
-# def index():
-#     return '<h1>Hello World</h1>'
-
-# @app.route('/user/<name>')
-# def user(name):
-#     return '<h1>Hello %s</h1>' % name
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -39,10 +30,6 @@ def index():
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))
-
-# @app.route('/user/<name>')
-# def user(name):
-#     return render_template('user.html', name=name)
 
 @app.errorhandler(404)
 def page_not_found(e):
