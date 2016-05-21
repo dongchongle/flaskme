de237511a19ecc107d17ec3447c05e30fa7dacfc
VimDong <dongchongle@gmail.com>
2016-05-21 16:29:48 +0800

    Email support

--- config.py
+++ config.py
@@ -4,8 +4,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hasrd to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKME_MAIL_SUBJECT_PREFIX = '[Flaskme]'
-    FLASKME_MAIL_SENDER = 'flaskme Admin <flaskme@example.com>'
+    FLASKME_MAIL_SENDER = 'Flaskme Admin <flaskme@example.com>'
     FLASKME_ADMIN = os.environ.get('FLASKME_ADMIN')
 
     @staticmethod
@@ -14,11 +19,6 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    MAIL_SERVER = 'smto.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
