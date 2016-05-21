fb9925197e0bc96b2b02744679188581271c53af
VimDong <dongchongle@gmail.com>
2016-05-21 12:26:07 +0800

    Email suport

--- hello.py
+++ hello.py
@@ -13,6 +13,7 @@ from wtforms.validators import Required
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.mail import Mail, Message
+from threading import Thread
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -39,11 +40,18 @@ app.config['FLASKME_ADMIN'] = os.environ.get('FLASKME_ADMIN')
 app.config['FLASKME_MAIL_SUBJECT_PREFIX'] = '[Flaskme]'
 app.config['FLASKME_MAIL_SENDER'] = 'Flaskme Admin <dongchongle@gmail.com>'
 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
 def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['FLASKME_MAIL_SUBJECT_PREFIX']+subject, sender=app.config['FLASKME_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template+'.txt', **kwargs)
     msg.html = render_template(template+'.html', **kwargs)
-    mail.send(msg)
+    # mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 class Role(db.Model):
     __tablename__ = 'roles'
