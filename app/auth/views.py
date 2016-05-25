8d3b018d1314e38c1f6be0a58c975aeb0528d71e
VimDong <dongchongle@gmail.com>
2016-05-25 11:13:25 +0800

    Account confirmation
    Update

--- app/auth/views.py
+++ app/auth/views.py
@@ -64,7 +64,7 @@ def confirm(token):
 
 @auth.route('/confirm')
 @login_required
-def resend_confirm():
+def resend_confirmation():
     token = current_user.generate_confirmed_token()
     send_email(current_user.email, 'Confirm Your Account', 'auth/email/confirm', user=current_user, token=token)
     flash('A new confirmation email has been sent to you by email.')
